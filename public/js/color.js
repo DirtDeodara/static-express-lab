594fd8483308083759ad50d61525dfafae9989ff
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-10 17:34:50 -0700

    got that dang thing to work

--- public/js/color.js
+++ public/js/color.js
@@ -4,11 +4,10 @@ const search = new URLSearchParams(window.location.search);
 const name = search.get('name');
 const h2 = document.createElement('h2');
 
-
 fetch(`/api/v1/colors/${name}`)
   .then(res => res.json())
   .then(color => {
-    h2.textContent = `${color.name}`;
+    h2.textContent = `${color.name} hex= ${color.hex}`;
   });
 
 root.appendChild(h2);
