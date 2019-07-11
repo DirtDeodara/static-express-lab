a84a4b4cff3c6a7881e24863ac56a9c9505182ef
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-11 09:31:16 -0700

    fixed typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { app } = require('./lib/app');
 
-const PORT = process.env.port || 9999;
+const PORT = process.env.PORT || 9999;
 
 app.listen(PORT, () => {
   console.log(`Vigilantly listening on port ${PORT}`);
