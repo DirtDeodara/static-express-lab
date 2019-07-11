4d80c3cf21bec035bcf99dc09040e78155ab2fe3
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-10 17:07:04 -0700

    able to get a json when I type in a color name

---
+++ public/js/color.js
@@ -0,0 +1,14 @@
+const root = document.getElementById('root');
+const search = new URLSearchParams(window.location.search);
+
+const name = search.get('name');
+const h2 = document.createElement('h2');
+
+
+fetch(`/api/v1/colors/${name}`)
+  .then(res => res.json())
+  .then(color => {
+    h2.textContent = `${color.name}`;
+  });
+
+root.appendChild(h2);
