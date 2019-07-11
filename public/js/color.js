a579b3f1c2c1d7ff76a6cb47c8c9dad6a5c1dd18
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-10 17:37:01 -0700

    changed an h2 to a p

--- public/js/color.js
+++ public/js/color.js
@@ -2,12 +2,12 @@ const root = document.getElementById('root');
 const search = new URLSearchParams(window.location.search);
 
 const name = search.get('name');
-const h2 = document.createElement('h2');
+const p = document.createElement('p');
 
 fetch(`/api/v1/colors/${name}`)
   .then(res => res.json())
   .then(color => {
-    h2.textContent = `${color.name} hex= ${color.hex}`;
+    p.textContent = `${color.name} HEX = ${color.hex}`;
   });
 
-root.appendChild(h2);
+root.appendChild(p);
